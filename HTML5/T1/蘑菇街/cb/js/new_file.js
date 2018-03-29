f66463e0ec66ca206ca106729faa7c65ce6a16ac
sunflower516 <2120311596@qq.com>
2018-03-29 17:39:01 +0800

    17 033郑小翠 作业
    Signed-off-by: sunflower516 <2120311596@qq.com>

---
+++ HTML5/T1/蘑菇街/cb/js/new_file.js
@@ -0,0 +1,8 @@
+function xs(name){
+	 var div= document.getElementById(name);
+     div.style.display="block";
+}
+function yc(name){
+	 var div= document.getElementById(name);
+	 div.style.display="none"
+}
